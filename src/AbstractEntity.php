159d31813b996eff7582f5651dd39f11b96ff089
gugglegum <gugglegum@gmail.com>
2017-11-01 13:22:07 +0500

    Many improvements for version 1.1.0:
     * Added possibility to set user-defined exception class for all exceptions generated by AbstractEntity. This may be
       very useful when you make some component and you don't want to give away details of the internal implementation.
       Your component usually have its own exception class and this is all what user using your component need to know.
       User shouldn't keep in mind about AbstractEntity you used to catch all exceptions from your component.
     * The `getAttributeNames()` method now returns only non-static properties, i.e. if you model class or it's parent
       class has static property -- it will not be included in list.
     * Eliminated static variables from `getAttributeNames()` and `hasAttribute()`. Static variables may result
       unexpected behavior if static variable in parent class initialized before child class loaded. Now we use
       class static properties with 2-dimension array where first level is name of concrete model class.
     * The `hasAttribute()` now is a static method. Being static you still able to call it via non-static calls.

--- src/AbstractEntity.php
+++ src/AbstractEntity.php
@@ -7,19 +7,50 @@ namespace gugglegum\AbstractEntity;
 /**
  * Base class for entities, provides basic methods for accessing attributes via getters and setters, contains
  * reflection to convert attribute name into getter or setter. Every attribute must have both getter and setter.
- * Every entity must have static method `getAttributeNames()` which returns the list of attributes. It's used in
- * `toArray()` method. Attributes may be stored as private properties or as associative array property - it's
- * fully defined by getters and setters.
+ * To work properly the method `getAttributeNames()` should return complete list (array) of attribute names
+ * (it's needed in `toArray()` method). This method already implemented in this base class, it uses the `ReflectionClass`
+ * to retrieve all class properties (including inherited properties) and returns its names. So your getters and setters
+ * should work with these properties. You may override this method to add some attributes not listed in properties
+ * or to remove some attributes that should not be referenced with some properties. If you would like to store
+ * attributes not in class properties (for example, in one single property with associative array) you should redefine
+ * `getAttributeNames()` method and make all getters and setters to store data as you want.
  *
  * @package gugglegum\AbstractEntity
  */
 abstract class AbstractEntity
 {
     /**
+     * Custom exception class. By default \gugglegum\AbstractEntity\Exception class will be thrown on error. But you
+     * may define your own exception class for your models.
+     *
+     * This property beginning from "__" to reduce possible collision with user-defined attribute names.
+     *
+     * @var string|null
+     */
+    private $__exceptionClass;
+
+    /**
+     * A two-dimensional associative array contains keys with concrete class models on 1st level and ordered array with
+     * list of attribute names. These lists are populated by `getAttributeNames()` method and then used as a cache for
+     * list to do not retrieve attributes every time using `\ReflectionClass`.
+     *
+     * @var array
+     */
+    private static $attributeNames = [];
+
+    /**
+     * The same as `attributeNames` but list of attribute names represented as associative array with NULL values. It's
+     * used by `hasAttribute()` method. Usage of associative array little bit faster than ordered array to search by
+     * value.
+     *
+     * @var array
+     */
+    private static $attributeNamesKeys = [];
+
+    /**
      * Constructor allows to initialize attribute values
      *
      * @param array $data           Associative array with [attribute => value] pairs
-     * @throws Exception
      */
     public function __construct(array $data = [])
     {
@@ -27,11 +58,34 @@ abstract class AbstractEntity
     }
 
     /**
+     * Returns currently used exception class if some error occurs.
+     *
+     * @return string
+     */
+    public function __getExceptionClass(): string
+    {
+        if ($this->__exceptionClass !== null) {
+            return $this->__exceptionClass;
+        } else {
+            return Exception::class;
+        }
+    }
+
+    /**
+     * Sets alternative user-defined exception class
+     *
+     * @param string $__exceptionClass
+     */
+    public function __setExceptionClass($__exceptionClass)
+    {
+        $this->__exceptionClass = $__exceptionClass;
+    }
+
+    /**
      * Creates new entity instance and initializes it with values from array. Actually shortcut for constructor.
      *
      * @param array $data Associative array with [attribute => value] pairs
      * @return static
-     * @throws Exception
      */
     public static function fromArray(array $data)
     {
@@ -43,7 +97,6 @@ abstract class AbstractEntity
      *
      * @param array $data Associative array with [attribute => value] pairs
      * @return self
-     * @throws Exception
      */
     public function setFromArray(array $data): self
     {
@@ -64,7 +117,9 @@ abstract class AbstractEntity
         $reflectionClass = new \ReflectionClass($className);
         $attributeNames = [];
         foreach ($reflectionClass->getProperties() as $property) {
-            $attributeNames[] = $property->getName();
+            if (!$property->isStatic() && $property->getName() !== '__exceptionClass') {
+                $attributeNames[] = $property->getName();
+            }
         }
         return $attributeNames;
     }
@@ -76,18 +131,17 @@ abstract class AbstractEntity
      */
     public static function getAttributeNames(): array
     {
-        static $attributes;
-
-        if ($attributes === null) {
-            $attributes = [];
+        if (!array_key_exists(static::class, self::$attributeNames)) {
+            self::$attributeNames[static::class] = [];
             $class = static::class;
             do {
-                $attributes = array_merge(self::getClassProperties($class), $attributes);
+                self::$attributeNames[static::class] = array_merge(self::getClassProperties($class), self::$attributeNames[static::class]);
                 $class = get_parent_class($class);
             } while ($class !== false);
-            $attributes = array_unique($attributes);
+            // Remove duplicates if some child class contains the same property as parent class
+            self::$attributeNames[static::class] = array_unique(self::$attributeNames[static::class]);
         }
-        return $attributes;
+        return self::$attributeNames[static::class];
     }
 
     /**
@@ -96,13 +150,12 @@ abstract class AbstractEntity
      * @param string $key
      * @return bool
      */
-    public function hasAttribute(string $key): bool
+    public static function hasAttribute(string $key): bool
     {
-        static $attributesHash;
-        if ($attributesHash === null) {
-            $attributesHash = array_fill_keys(static::getAttributeNames(), null);
+        if (!array_key_exists(static::class, self::$attributeNamesKeys)) {
+            self::$attributeNamesKeys[static::class] = array_fill_keys(static::getAttributeNames(), null);
         }
-        return array_key_exists($key, $attributesHash);
+        return array_key_exists($key, self::$attributeNamesKeys[static::class]);
     }
 
     /**
@@ -110,12 +163,12 @@ abstract class AbstractEntity
      *
      * @param string $key
      * @return mixed
-     * @throws Exception
      */
     public function getAttribute(string $key)
     {
         if (!$this->hasAttribute($key)) {
-            throw new Exception("Attempt to get non-existing attribute \"{$key}\"");
+            $exceptionClass = $this->__getExceptionClass();
+            throw new $exceptionClass("Attempt to get non-existing attribute \"{$key}\"");
         }
         $getter = $this->getGetter($key);
         return $this->{$getter}();
@@ -127,12 +180,12 @@ abstract class AbstractEntity
      * @param string $key
      * @param mixed  $value
      * @return self
-     * @throws Exception
      */
     public function setAttribute(string $key, $value): self
     {
         if (!$this->hasAttribute($key)) {
-            throw new Exception("Attempt to set non-existing attribute \"{$key}\"");
+            $exceptionClass = $this->__getExceptionClass();
+            throw new $exceptionClass("Attempt to set non-existing attribute \"{$key}\"");
         }
         $setter = $this->getSetter($key);
         $this->{$setter}($value);
@@ -143,7 +196,6 @@ abstract class AbstractEntity
      * Returns entity as associative array. Key of array is attribute names.
      *
      * @return array                Associative array with [attributeName => attributeValue] pairs
-     * @throws Exception
      */
     public function toArray(): array
     {
@@ -174,7 +226,6 @@ abstract class AbstractEntity
      *
      * @param string $attributeName
      * @return string
-     * @throws Exception
      */
     protected function getGetter(string $attributeName): string
     {
@@ -183,7 +234,8 @@ abstract class AbstractEntity
         } elseif (method_exists($this, ($getter2 = 'is' . ucfirst($attributeName)))) {
             return $getter2;
         } else {
-            throw new Exception("Can't find getter method {$getter1}() or {$getter2}() for attribute \"{$attributeName}\" in " . get_class($this));
+            $exceptionClass = $this->__getExceptionClass();
+            throw new $exceptionClass("Can't find getter method {$getter1}() or {$getter2}() for attribute \"{$attributeName}\" in " . get_class($this));
         }
     }
 
@@ -192,13 +244,13 @@ abstract class AbstractEntity
      *
      * @param string $attributeName
      * @return string
-     * @throws Exception
      */
     protected function getSetter(string $attributeName): string
     {
         $setter = 'set' . ucfirst($attributeName);
         if (!method_exists($this, $setter)) {
-            throw new Exception("Can't find setter method {$setter}() for attribute \"{$attributeName}\" in " . get_class($this));
+            $exceptionClass = $this->__getExceptionClass();
+            throw new $exceptionClass("Can't find setter method {$setter}() for attribute \"{$attributeName}\" in " . get_class($this));
         }
         return $setter;
     }
